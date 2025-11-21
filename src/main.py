ddd633f99f1c6fd68a72195e52d83079a5d8a94b
Lucas Víctor Ferreira de Araújo <120221548+Lucas13032003@users.noreply.github.com>
2025-11-21 18:28:37 -0300

    rotas

--- src/main.py
+++ src/main.py
@@ -1,31 +1,33 @@
 from fastapi import FastAPI
 
-from src.colecao.router import router as colecao_router
+from fastapi.middleware.cors import CORSMiddleware
 
-# importar SEED
+from src.colecao.router import router as colecao_router
 from src.colecao.seed import seed_colecao
 from src.db.session import Base, engine, get_db
 from src.palpites.router import router as palpites_router
 from src.partidas.router import router as partidas_router
 from src.ranking.router import router as ranking_router
-
-# importar routers
 from src.usuario.router import router as user_router
 
 app = FastAPI()
 
+# ---- CORS FIX ----
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5174", "http://localhost:5173", "*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+# --------------------
 
 @app.on_event("startup")
 def startup():
-    # criar tabelas
     Base.metadata.create_all(bind=engine)
-
-    # executar seed
     db = next(get_db())
     seed_colecao(db)
 
-
-# incluir rotas
 app.include_router(user_router)
 app.include_router(palpites_router)
 app.include_router(partidas_router)
